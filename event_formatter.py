184ee0e6ffabd89c73bb0026560b45c36c1ab7f0
caesarakalaeii <caesarlp@gmx.de>
2023-06-22 00:13:39 +0200

    not yet tested

--- event_formatter.py
+++ event_formatter.py
@@ -7,6 +7,7 @@ class Formatter(object):
     def write_events(events):
         with open("credits.txt", "w") as file:
             for event, users in events:
-                file.write(f"{event}\n")
-                for user in users:
-                    file.write(f"{user}, ")
+                if len(users) <0:
+                    file.write(f"{event}: \n")
+                    for user in users:
+                        file.write(f"{user}, ")
